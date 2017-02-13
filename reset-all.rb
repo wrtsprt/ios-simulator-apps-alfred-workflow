8c95cb3c1b6efe1cb5cc68293b5f80a1491dd183
Raphael Doehring <raphael.doehring@vasco.com>
2017-02-13 17:17:26 +0100

    Split reset in Reset current Xcode and reset all Xcode’s …
    Potentially this doesn’t work 100% for multiple installed Xcode’s.

--- reset-all.rb
+++ reset-all.rb
@@ -1,20 +1,29 @@
 #!/bin/env ruby
 xcodes = Dir.glob("/Applications/Xcode*.app")
-puts xcodes
+
+puts "all xcodes #{xcodes}"
+
+
+current_developer_dir = %x{xcode-select -p}.chomp
+puts "current dev dir: #{current_developer_dir}"
+
+xcrun_path = "/usr/bin/xcrun"
 xcodes.each do |xcode|
-	simctl = "#{xcode}/Contents/Developer/usr/bin/xcrun simctl"
-	puts "#{simctl} list"
-	out = %x{#{simctl} list}
-	out.lines.each do |line|
-		match = /(\s+)(.*)\s\(([\d\w]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12})\)/.match(line)
-		if match
-			id = match[3]
-			name = match[2]
-			puts "Erasing #{name} (#{id})"
-			result = system("#{simctl} erase #{id}")
-			if not result
-				puts "Failed to erase #{name} - #{id}"
-			end
-		end
+
+    puts "----------------> #{xcode} ---------------------->"
+	developer_dir = "#{xcode}/Contents/Developer"
+
+	simctl = if developer_dir == current_developer_dir
+	  "#{xcrun_path} simctl erase all"
+	else
+	  "DEVELOPER_DIR=#{developer_dir} #{xcrun_path} simctl erase all"
 	end
+
+	puts "removing core service"
+	remove_core_service = "launchctl remove com.apple.CoreSimulator.CoreSimulatorService || true"
+	output1 = %x{#{remove_core_service}}
+
+	puts simctl
+	output = %x{#{simctl}}
+	puts output 
 end
